50b17387ca0824f0c2746fe24dc86b69ec4e890a
Jay Jani <jayjani002@gmail.com>
2022-11-20 16:56:32 +0530

    Connected to Mongo Atlas

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const path = require('path');
+
+// Mongo Atlas connection:
+const { MongoStore } = require('connect-mongo');
+const MongoDBStore = require('connect-mongo');
 
 // models;
 const User = require('./models/UserModel');
@@ -26,12 +34,14 @@ const LocalStrategy = require('passport-local');
 //method-override package so that we can make use of put and delete
 const methodOverride = require('method-override');
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/sma';
+console.log(dbUrl);
 const mongoose = require('mongoose');
 main()
   .then((res) => {})
   .catch((err) => {});
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/sma');
+  await mongoose.connect(dbUrl);
 }
 
 // telling express that we will be using ejsMate instead of the default one
@@ -55,10 +65,22 @@ app.use(methodOverride('_method'));
 //for making use of custom css and js files in the public directory:
 app.use(express.static(path.join(__dirname, 'public')));
 
-const secret = 'thisshouldbeabettersecret';
+const secret = process.env.secret || 'thisshouldbeabettersecret';
+const store = MongoDBStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret,
+  },
+   // should be in seconds:
+   touchAfter: 24 * 60 * 60,
+});
+store.on('error', function (e) {
+  console.log('Session store error: ', e);
+});
 
 // express-session settings
 const sessionConfig = {
+  store,
   name: 'session',
   secret,
   resave: false,
@@ -107,8 +129,9 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, (req, res) => {
-  console.log('Active port 3000');
+const port = process.env.PORT || 3000
+app.listen(port, (req, res) => {
+  console.log(`SMA active on port ${port}`);
 });
 
 // for testing
